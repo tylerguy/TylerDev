2a091a4ee4bb6bb5aae37a149c4aa1338eb53f28
tylerguy <tylerdenman200@gmail.com>
2022-10-25 19:41:26 +0100

    Initial Commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'TylerDev';
+export const SITE_DESCRIPTION = 'A blog about my web development journey and other things.';
