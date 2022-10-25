a77064ffbf7c469c003c95d99f47f54c58a15f52
tylerguy <tylerdenman200@gmail.com>
2022-10-25 20:06:08 +0100

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://tylerdev.me",
-  base: "/Tyler.dev",
   integrations: [mdx(), sitemap()],
 });
