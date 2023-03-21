0de6d6b23eea811d2356c15c896e937ef5676c32
tylerguy <tylerdenman200@gmail.com>
2023-03-21 09:55:48 +0000

    Add link to project page

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'TylerDev';
-export const SITE_DESCRIPTION = 'A blog about my web development journey and other things.';
+export const SITE_DESCRIPTION = 'My Personal Portfolio and Blog';
