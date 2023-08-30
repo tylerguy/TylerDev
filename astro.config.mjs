c5f9c9988123116f141c43bfcd8b92c0e2ba405c
Tyler Denman <tylerdenman200@gmail.com>
2023-08-30 11:22:32 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://tylerguy.github.io",
-  base: "/Tyler.dev",
   integrations: [mdx(), sitemap()],
 });
