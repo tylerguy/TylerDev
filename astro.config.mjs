9f7198de2a94203e6a3e376f41a91af39c667dad
tylerguy <tylerdenman200@gmail.com>
2023-11-08 14:32:15 +0100

    Add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tylerdev.space",
-  integrations: [mdx(), sitemap()],
-
+  integrations: [mdx(), sitemap(), react()],
   build: {
     rollupOptions: {
-      external: ["@astro/types"], // Add "@astro/types" to the external modules
-    },
-  },
+      external: ["@astro/types"] // Add "@astro/types" to the external modules
+    }
+  }
 });
