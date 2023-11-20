dc33679073b9f9068d7b959e9e373076af106fc6
tylerguy <tylerdenman200@gmail.com>
2023-11-20 20:24:04 +0100

    Add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import partytown from '@astrojs/partytown'
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://tylerdev.space",
-  integrations: [mdx(), sitemap(), react(), 
-                partytown({
-			config: {
-			  forward: ["dataLayer.push"],
-			},
-		}),
-                 ],
+  integrations: [mdx(), sitemap(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), compress()],
   build: {
     rollupOptions: {
       external: ["@astro/types"] // Add "@astro/types" to the external modules
