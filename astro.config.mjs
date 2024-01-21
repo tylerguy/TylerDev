f243d370384b44e2e3196c54c3e77a0ef145a915
Tyler Denman <tylerdenman200@gmail.com>
2024-01-21 12:34:15 +0000

    switch to serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from '@astrojs/partytown';
-import vercel from '@astrojs/vercel/serverless';
+import vercelServerless from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 import compress from "astro-compress";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,5 +25,5 @@ export default defineConfig({
     }
   },
   output: 'server',
-    adapter: vercel(),
+  adapter: vercelServerless()
 });
