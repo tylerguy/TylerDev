0aa6eae966e4f25540ee2886c8de2e22b97bfaaf
tylerguy <tylerdenman200@gmail.com>
2024-05-06 16:40:20 +0100

    formatting changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,21 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import partytown from '@astrojs/partytown';
-// import vercel from '@astrojs/vercel/serverless';
-
+import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import compress from "astro-compress";
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://tylerdev.space",
-  integrations: [mdx(), sitemap(), react(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), compress()],
- // output: 'hybrid',
-/*  adapter: vercel({
-    edgeMiddleware: true,
-  }), */
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    compress(),
+  ],
 });
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'TylerDev';
-export const SITE_DESCRIPTION = 'My Personal Portfolio and Blog';
-export const HEADER_COLOR = ''
+export const SITE_TITLE = "TylerDev";
+export const SITE_DESCRIPTION = "My Personal Portfolio and Blog";
+export const HEADER_COLOR = "";
